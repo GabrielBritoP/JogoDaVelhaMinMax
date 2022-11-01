accf3f62e376526fa51f89879c2334afaf280e7d
Gabriel Brito <iamgabrielbp@gmail.com>
2022-11-01 01:23:24 -0300

    first commit

---
+++ classes/jogador.js
@@ -0,0 +1,94 @@
+import Board from './board.js';
+
+export default class Jogador {
+    constructor(profundidadeMax = -1) {
+        this.profundidadeMax = profundidadeMax;
+        this.nodesMap = new Map();
+    }
+
+    getMelhorMovimento(board, maximizing = true, callback = () => {
+    }, profundidade = 0) {
+
+        if (profundidade == 0) this.nodesMap.clear();
+
+        if (board.isTerminal() || profundidade === this.profundidadeMax) {
+            if (board.isTerminal().winner === 'x') {
+                return 100 - profundidade;
+            } else if (board.isTerminal().winner === 'o') {
+                return -100 + profundidade;
+            }
+            return 0;
+        }
+        // maximimacao alpha
+        if (maximizing) {
+
+            let best = -100;
+
+            board.getAvailableMoves().forEach(index => {
+
+                const child = new Board([...board.state]);
+
+                child.insert('x', index);
+                //Recursivamente chamando o melhor movimento incrementando a profundidade comparando com o estado do campo
+                const nodeValue = this.getMelhorMovimento(child, false, callback, profundidade + 1);
+                best = Math.max(best, nodeValue);
+            });
+
+            if (profundidade == 0) {
+                let returnValue;
+                if (typeof this.nodesMap.get(best) == 'string') {
+                    const arr = this.nodesMap.get(best).split(',');
+                    const rand = Math.floor(Math.random() * arr.length);
+                    returnValue = arr[rand];
+                } else {
+                    returnValue = this.nodesMap.get(best);
+                }
+
+                callback(returnValue);
+                return returnValue;
+
+                return best;
+            }
+            // minimizacao beta
+            if (!maximizing) {
+
+                let best = 100;
+
+                board.getAvailableMoves().forEach(index => {
+
+                    const child = new Board([...board.state]);
+
+
+                    child.insert('o', index);
+
+                    //Recursively calling getMelhorMovimento this time with the new board and maximizing turn and incrementing the profundidade
+                    let nodeValue = this.getMelhorMovimento(child, true, callback, profundidade + 1);
+                    //Updating best value
+                    best = Math.min(best, nodeValue);
+
+                    if (profundidade == 0) {
+
+                        const moves = this.nodesMap.has(nodeValue) ? this.nodesMap.get(nodeValue) + ',' + index : index;
+                        this.nodesMap.set(nodeValue, moves);
+                    }
+                });
+
+                if (profundidade == 0) {
+                    let returnValue;
+                    if (typeof this.nodesMap.get(best) == 'string') {
+                        const arr = this.nodesMap.get(best).split(',');
+                        const rand = Math.floor(Math.random() * arr.length);
+                        returnValue = arr[rand];
+                    } else {
+                        returnValue = this.nodesMap.get(best);
+                    }
+
+                    callback(returnValue);
+                    return returnValue;
+                }
+
+                return best;
+            }
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,77 @@
+import Board from './classes/board.js';
+import Jogador from './classes/jogador.js';
+import {addClass, drawWinningLine, hasClass} from './helpers.js';
+
+function newGame(profundidade = 3, startingJogador = 1) {
+    console.log(profundidade)
+    const jogador = new Jogador(parseInt(profundidade));
+    const board = new Board(['', '', '', '', '', '', '', '', '']);
+
+    const boardDIV = document.getElementById("board");
+    boardDIV.className = '';
+    boardDIV.innerHTML =
+        `<div class="cells-wrap">
+            <button class="cell-0"></button>
+            <button class="cell-1"></button>
+            <button class="cell-2"></button>
+            <button class="cell-3"></button>
+            <button class="cell-4"></button>
+            <button class="cell-5"></button>
+            <button class="cell-6"></button>
+            <button class="cell-7"></button>
+            <button class="cell-8"></button>
+        </div>`;
+
+    const htmlCells = [...boardDIV.querySelector('.cells-wrap').children];
+
+    const starting = parseInt(startingJogador),
+        maximizando = starting;
+    let jogadorTurn = starting;
+
+    if (!starting) {
+        const centerAndCorners = [0, 2, 4, 6, 8];
+        const firstChoice = centerAndCorners[Math.floor(Math.random() * centerAndCorners.length)];
+        const symbol = !maximizando ? 'x' : 'o';
+        board.insert(symbol, firstChoice);
+        addClass(htmlCells[firstChoice], symbol);
+        jogadorTurn = 1;
+    }
+
+    board.state.forEach((cell, index) => {
+        htmlCells[index].addEventListener('click', () => {
+
+            if (hasClass(htmlCells[index], 'x') || hasClass(htmlCells[index], 'o') || board.isTerminal() || !jogadorTurn) return false;
+            const symbol = maximizando ? 'x' : 'o';
+
+            board.insert(symbol, index);
+            addClass(htmlCells[index], symbol);
+
+            if (board.isTerminal()) {
+                drawWinningLine(board.isTerminal());
+            }
+            jogadorTurn = 0;
+
+            jogador.getMelhorMovimento(board, !maximizando, best => {
+                const symbol = !maximizando ? 'x' : 'o';
+                board.insert(symbol, parseInt(best));
+                addClass(htmlCells[best], symbol);
+                if (board.isTerminal()) {
+                    drawWinningLine(board.isTerminal());
+                }
+                jogadorTurn = 1;
+            });
+        }, false);
+        if (cell) addClass(htmlCells[index], cell);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const profundidade = -1;
+    const startingJogador = 1;
+    newGame(profundidade, startingJogador);
+    document.getElementById("newGame").addEventListener('click', () => {
+        const startingDIV = document.getElementById("starting");
+        const starting = startingDIV.options[startingDIV.selectedIndex].value;
+        newGame(3, starting);
+    });
+});
